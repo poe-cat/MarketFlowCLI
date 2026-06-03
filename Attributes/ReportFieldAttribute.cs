cebff3149a2aa590ff2d6806fbebb91c38f4e08b
Katarzyna Szewiel <84228264+poe-cat@users.noreply.github.com>
2026-06-03 13:20:12 +0200

    add comment ReportFieldAttribute.cs

--- Attributes/ReportFieldAttribute.cs
+++ Attributes/ReportFieldAttribute.cs
@@ -1,12 +1,12 @@
 namespace MarketFlowCLI.Attributes;
 
+// Atrybut oznaczający właściwości/pola, które mają być wyświetlane przez ReflectionPrinter
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
-public sealed class ReportFieldAttribute : Attribute
-{
+public sealed class ReportFieldAttribute : Attribute {
+
     public string Label { get; }
 
-    public ReportFieldAttribute(string label)
-    {
+    public ReportFieldAttribute(string label) {
         Label = label;
     }
 }
